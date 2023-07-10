9dd377b91834796165e6567afc503935c5328879
Mike <reudymiguel20@gmail.com>
2023-07-10 00:37:42 -0400

    Stage 2/5: Requested dog breed

--- src/index.js
+++ src/index.js
@@ -1,22 +1,64 @@
 const content = document.getElementById('content');
-const button = document.getElementById('button-random-dog');
+const buttonRandomDog = document.getElementById('button-random-dog');
+const buttonShowBreed = document.getElementById('button-show-breed');
+const inputBreed = document.getElementById('input-breed');
 
-button.addEventListener('click', function() {
+<!--Button Events-->
+buttonRandomDog.addEventListener('click', function () {
     content.innerHTML = ''; // clear content
     getDogImage();
 });
 
+buttonShowBreed.addEventListener('click', function () {
+    content.innerHTML = ''; // clear content
+    getBreedImage();
+});
+
+
+<!--Functions-->
+function getBreed() {
+    return inputBreed.value;
+}
+
+// get dog image from api and append to content div
 async function getDogImage() {
-    try{
+    try {
+        // 1. fetch() is a function that returns a promise
+        // 2. await is used to wait for the promise to resolve
+        // 3. response.json() is a function that returns a promise
+        // 4. await is used to wait for the promise to resolve
+        // 5. data.message is the url of the image
         const response = await fetch('https://dog.ceo/api/breeds/image/random');
         const data = await response.json();
         const img = document.createElement('img');
         img.src = data.message;
 
         return content.appendChild(img);
-    }
-    catch {
+    } catch {
         console.log('error');
     }
 
+}
+
+// get specific breed image from api and append to content div
+async function getBreedImage() {
+    // get breed from input and convert to lowercase
+    let breed = getBreed().toLowerCase();
+
+    // fetch image from api and convert to json format
+    const response = await fetch(`https://dog.ceo/api/breed/${breed}/images/random`);
+    const data = await response.json();
+
+    // create img element and set src to image url
+    const img = document.createElement('img');
+    img.src = data.message;
+
+    // if breed not found, display error message, else display image
+    if (data.status === 'error') {
+        const notFound = document.createElement('p');
+        notFound.innerText = 'Breed not found!';
+        return content.appendChild(notFound);
+    } else {
+        return content.appendChild(img);
+    }
 }
